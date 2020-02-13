b1128e04411837a59fa223446dadd611e549affb
melvasquires <melva.a.squires@gmail.com>
2020-02-13 16:15:20 -0500

    aside to duck

---
+++ ducksJS.js
@@ -0,0 +1,23 @@
+// Exercise Web page
+// Make a web page.
+// Add html, head, and body tags
+// In the body add a header, article, section, footer
+// In each of those add some divs, paragraphs, and spans of text.
+// Apply classes and ids to html elements.
+
+// Exercise Links and Style
+// add an anchor tag that links to your favorite website.
+// wrap a div of text that links to your text document you created.
+// use an anchor to link to an id somewhere else on the page.
+// put a style tag in the head of your html and color an element using rgb, hex color, and a key word.
+// set the height/width, padding, border, and margin for some of your elements.
+// for an element that has a child:
+// put a px width/height on the parent.
+// on the child put a percentage width/height
+
+// Exercise Aside
+// add an html aside element and put it above your section
+// float the aside to the side of the section
+// use inline-block to make a 2 column layout (2 elements side by side)
+
+divWw.style.color="red"
